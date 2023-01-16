a99442b1ac8cb6d0dc0241fdc572d2e9e4e8dc87
Judalm Park (dami) <wnekfa1004@naver.com>
2023-01-16 19:44:52 +0900

    다양한 연관관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/jpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/jpaMain.java
@@ -20,14 +20,6 @@ public class jpaMain {
 
         try {
 
-            Order order = new Order();
-            em.persist(order);
-
-//            order.addOrderItem(new OrderItem());
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
 
 
 
